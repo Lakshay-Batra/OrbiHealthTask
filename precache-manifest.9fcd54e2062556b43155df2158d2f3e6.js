97b6dd72773499a112b26209dacf8fdb5ecf1cd0
Demo-Lakshay <50356418+Lakshay-Batra@users.noreply.github.com>
2020-10-26 04:18:56 +0530

    Updates

---
+++ precache-manifest.9fcd54e2062556b43155df2158d2f3e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c9613f89f3a2c3a95b4a39d80284c9",
+    "url": "/OrbiHealthTask/index.html"
+  },
+  {
+    "revision": "d2bd4f5d88517c7eed35",
+    "url": "/OrbiHealthTask/static/css/main.d7fb6431.chunk.css"
+  },
+  {
+    "revision": "cf0b2e4a00cd2bbae1f8",
+    "url": "/OrbiHealthTask/static/js/2.192bb8de.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/OrbiHealthTask/static/js/2.192bb8de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2bd4f5d88517c7eed35",
+    "url": "/OrbiHealthTask/static/js/main.956ec0f6.chunk.js"
+  },
+  {
+    "revision": "6f9e1760d52d3b4f3a8e",
+    "url": "/OrbiHealthTask/static/js/runtime-main.1401d416.js"
+  }
+]);
